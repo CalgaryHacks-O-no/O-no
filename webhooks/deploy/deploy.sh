a052d0617416f220a4037372ad9f1fd5e683a245
TopazJ <shamez.meghji1@ucalgary.ca>
2021-02-11 15:40:02 -0700

    Added webhooks folder and deployment script

---
+++ webhooks/deploy/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git fetch --all
+git checkout --force "origin/deploy"
+npm i
+npm run prod
+source ../bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+deactivate
+sudo systemctl stop gunicorn
+sudo systemctl start gunicorn
