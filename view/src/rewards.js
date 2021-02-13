bc2585d18d294d69e2fd524f0ec7528ccfd38dc7
erigb <ethanrigby@live.com>
2021-02-13 14:53:17 -0700

    Rewards

---
+++ view/src/rewards.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "regenerator-runtime/runtime.js"; // Global import for async/await keywords TODO: This doesn't look needed for view-side, make this only on edit-side. See issue #183
+
+//JS Code goes here. This will be used to get the List of coupons and gift cards from restaurants and apply it to the list like thing that Richard has.
+
+class Rewards extends React.Component  {
+
+
+    render = () => {
+        return (
+    <h1>Hey errybody</h1>
+        );
+    }
+}
