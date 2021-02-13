2f9dfc806135454c44e03c5d2727f09cce578cd8
R Lee <alpha.r.lee.dev@gmail.com>
2021-02-13 16:41:12 -0700

    Added purchase details

--- view/src/PurchaseScreen.jsx
+++ view/src/PurchaseScreen.jsx
@@ -1,98 +1,21 @@
 import React from "react";
+import Footer from "./Footer";
+import PurchaseDetails from "./PurchaseDetails";
+import PurchasePaymentForm from "./PurchasePaymentForm";
 
 function PurchaseScreen(props) {
-	const onSubmit = () => {
-		console.log("!!! submitted");
-	};
-
-	const basicField = (id, desc, type = "text") => (
-		<div className="form-group row">
-			<label htmlFor={id} className="col-sm-3 col-form-label">
-				{desc}
-			</label>
-			<div className="col">
-				<input
-					type={type}
-					className="form-control"
-					id={id}
-					aria-describedby={desc}
-				/>
-			</div>
-		</div>
-	);
-
 	return (
-		<div className="container">
-			<form onSubmit={onSubmit}>
-				{basicField("name", "Name")}
-				{basicField("email", "Email", "email")}
-				<div className="form-group">
-					<label htmlFor="name" className="mr-3">
-						Tip amount
-					</label>
-					<div
-						className="btn-group btn-group-toggle"
-						data-toggle="buttons"
-					>
-						<label className="btn btn-light active">
-							<input type="radio" name="options" id="option1" />
-							10%
-						</label>
-						<label className="btn btn-light">
-							<input type="radio" name="options" id="option2" />
-							15%
-						</label>
-						<label className="btn btn-light">
-							<input type="radio" name="options" id="option3" />
-							20%
-						</label>
-					</div>
-					<div className="form-group row">
-						<div className="col-sm-3">
-							<label
-								htmlFor="varTip"
-								className="mr-3 col-form-label"
-							>
-								Or choose your own tip
-							</label>
-						</div>
-						<div className="col">
-							<div className="input-group">
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Tip amount"
-									id="varTip"
-								/>
-								<div
-									className="input-group-append"
-									id="varTip-dollar"
-								>
-									<button
-										className="btn btn-outline-secondary"
-										type="button"
-									>
-										$
-									</button>
-									<button
-										className="btn btn-outline-secondary"
-										type="button"
-									>
-										%
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<small className="form-text text-muted">
-						100% of all funds go to your local restaurants 😊
-					</small>
+		<div className="container mb-5">
+			<div className="row">
+				<div className="col-lg-6">
+					<PurchaseDetails />
 				</div>
-
-				<button type="submit" className="btn btn-primary">
-					Submit
-				</button>
-			</form>
+				<div className="col-lg-6">
+					<div className="pl-4">
+						<PurchasePaymentForm />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- view/src/index.js
+++ view/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import PurchaseScreen from "./PurchaseScreen";
 import Cards from "./Cards";
 import "regenerator-runtime/runtime.js"; // Global import for async/await keywords TODO: This doesn't look needed for view-side, make this only on edit-side. See issue #183
+import Footer from "./Footer";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Cards />
 		<PurchaseScreen />
+		<Footer />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
