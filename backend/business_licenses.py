4978270720a08a118c14d6b4d3d0530dc58bd6f9
ArsenicMeatball <jeandavid.rousseau@ucalgary.ca>
2021-02-13 16:03:57 -0700

    updates to import script

--- backend/business_licenses.py
+++ backend/business_licenses.py
@@ -1,14 +1,14 @@
 import pandas
-import matplotlib
+# import matplotlib
 import backend.models as internal_models
 
 
-def read_business_licenses()->pandas.DataFrame:
+def read_business_licenses() -> pandas.DataFrame:
     df = pandas.read_csv('Calgary_Business_Licenses_Map.csv')
     return df
 
 
-def get_restaurant_indexes(df:pandas.DataFrame)->set:
+def get_restaurant_indexes(df: pandas.DataFrame) -> set:
     restaurant_idxs = set()
 
     # grab all food and grocery stores
@@ -19,7 +19,7 @@ def get_restaurant_indexes(df:pandas.DataFrame)->set:
     return restaurant_idxs
 
 
-def get_restaurant_communities(df:pandas.DataFrame, restaurant_idxs:set)->set:
+def get_restaurant_communities(df: pandas.DataFrame, restaurant_idxs: set) -> set:
     # separate by community
     community_names = {'default'}
     communities = df['COMDISTNM']
@@ -32,16 +32,14 @@ def get_restaurant_communities(df:pandas.DataFrame, restaurant_idxs:set)->set:
     return community_names
 
 
-def add_communities_to_database(names:set)->None:
+def add_communities_to_database() -> None:
+    df = read_business_licenses()
+    names = get_restaurant_communities(df, get_restaurant_indexes(df))
+    print(internal_models.Community.objects.all())
     for name in names:
         new_community = internal_models.Community(name=name)
         new_community.save()
 
-
-if __name__ == '__main__':
-    df = read_business_licenses()
-    print(internal_models.Community.objects.all())
-    add_communities_to_database(get_restaurant_communities(df, get_restaurant_indexes(df)))
     print(internal_models.Community.objects.all())
 
 
@@ -56,16 +54,16 @@ if __name__ == '__main__':
 # for idx in restaurant_idxs:
 #     name = names[idx].strip()
 #     print(name)
-    #
-    # community = communities[idx]
-    # if not isinstance(community, str):
-    #     continue
-    # community = community.split(',')[0].strip()
-    # print(community, type(community))
+#
+# community = communities[idx]
+# if not isinstance(community, str):
+#     continue
+# community = community.split(',')[0].strip()
+# print(community, type(community))
 
-    # if community not in community_restaurant:
-    #     community_restaurant[community] = []
-    # community_restaurant[community].append(name)
+# if community not in community_restaurant:
+#     community_restaurant[community] = []
+# community_restaurant[community].append(name)
 #
 #
 # print(len(community_restaurant))
