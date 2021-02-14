edef4bd17e26bb6d0b06a431ab20659cdf96e18e
victor <victor.sanchezalarco@ucalgary.ca>
2021-02-13 19:14:58 -0800

    Merged master into this branch

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('view/<str:model_name>/<uuid:model_id>', views.get_data),
     path('create/<str:model_name>', views.create_model),
     path('update/<str:model_name>/<uuid:model_id>', views.update_model),
+    path('update_communities', views.update_communities),
+    path('update_restaurants', views.update_restaurants),
     path('fuckit', views.imp)
 ]
 
