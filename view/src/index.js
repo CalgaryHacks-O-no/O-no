85e1f56a25c130ab88f9d53975b8ab9afe7befe3
R Lee <alpha.r.lee.dev@gmail.com>
2021-02-13 20:53:25 -0700

    Added PointsBreakdown

--- view/src/index.js
+++ view/src/index.js
@@ -5,10 +5,12 @@ import Cards from "./Cards";
 import MapContainer from "./Maps";
 import "regenerator-runtime/runtime.js"; // Global import for async/await keywords TODO: This doesn't look needed for view-side, make this only on edit-side. See issue #183
 import Footer from "./Footer";
+import PointsBreakdown from "./PointsBreakdown";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Cards />
+		<PointsBreakdown />
 		<PurchaseScreen />
 		<MapContainer />
 		<Footer />
