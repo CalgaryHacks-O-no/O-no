27b7084df1257380a5f9772e032bb2cafd917b82
erigb <ethanrigby@live.com>
2021-02-13 20:53:20 -0700

    Leaderboards More like LeaderDone

--- view/src/index.js
+++ view/src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PurchaseScreen from "./PurchaseScreen";
 import Cards from "./Cards";
-import MapContainer from "./Maps";
 import "regenerator-runtime/runtime.js"; // Global import for async/await keywords TODO: This doesn't look needed for view-side, make this only on edit-side. See issue #183
 import Footer from "./Footer";
+import MapLeaderboardScreen from "./MapLeaderboardScreen";
 
 ReactDOM.render(
 	<React.StrictMode>
+		<MapLeaderboardScreen />
 		<Cards />
 		<PurchaseScreen />
-		<MapContainer />
 		<Footer />
 	</React.StrictMode>,
 	document.getElementById("root")
